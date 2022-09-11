7f696bfb3420c63dc0690ba6f3ed940c744c3da4
Ning Shang <syncom.dev@gmail.com>
2022-09-11 14:56:15 -0700

    Use tstd crate
    This is to reproduce an issue related to a recent tstd-rs change for
    aarch64 build.
    
    The problematic change in tstd is
    https://github.com/gyscos/zstd-rs/commit/f0d8a12f0f520095cf26a0dac35b8d0939277add.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate rand;
+extern crate zstd;
 use rand::Rng;
 
 fn main() {
     let rn = rand::thread_rng().gen_range(38, 45);
     println!("Hello, random: {}", rn);
+    println!("ZSTD default compression level: {}", zstd::DEFAULT_COMPRESSION_LEVEL);
 }
