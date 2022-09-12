33005fbcd4a1be0f604afbf1415b51004dda1d70
Ning Shang <syncom.dev@gmail.com>
2022-09-11 18:45:00 -0700

    Use tstd crate (#11)
    * Use tstd crate
    
    This is to reproduce an issue related to a recent tstd-rs change for
    aarch64 build.
    
    The problematic change in tstd is
    https://github.com/gyscos/zstd-rs/commit/f0d8a12f0f520095cf26a0dac35b8d0939277add.
    
    * Repro tstd-rs linking issue
    
    In `Cargo.tomol`
    
    - Uncomment line for zstd rev="56c8b52b6d448e376e746fc062077f68febc03a3": builds successfully
    - Uncomment line for zstd rev="f0d8a12f0f520095cf26a0dac35b8d0939277add": causes build failure
    
    Build commands (with `cross-rs`)
    
         cross build --target=x86_64-unknown-linux-musl
         cross build --target=aarch64-unknown-linux-musl
    
    * Use zstd-rs fork that contains r10e patch for demo
    
    Use a branch that has the reproducibility improvement https://github.com/gyscos/zstd-rs/pull/162

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 extern crate rand;
+extern crate zstd;
+use std::io::Write;
+
 use rand::Rng;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let rn = rand::thread_rng().gen_range(38, 45);
     println!("Hello, random: {}", rn);
+    println!("ZSTD default compression level: {}", zstd::DEFAULT_COMPRESSION_LEVEL);
+
+    let mut infile = std::fs::File::create("infile")?;
+    let mut outfile = std::fs::OpenOptions::new().write(true).create(true).open("outfile")?;
+    infile.write_all(b"Hello, random")?;
+    let infile1 = std::fs::File::open("infile")?;
+    zstd::stream::copy_encode(&infile1, &mut outfile, 0)?;
+
+    Ok(())
 }
