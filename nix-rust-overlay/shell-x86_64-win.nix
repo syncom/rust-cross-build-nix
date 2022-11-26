b1d84c225d482eb5f975018e45eff6477e7e8404
Ning Shang <syncom.dev@gmail.com>
2022-11-26 15:45:18 -0800

    rust-overlay r10e builds: support Windows (#14)

---
+++ nix-rust-overlay/shell-x86_64-win.nix
@@ -0,0 +1,23 @@
+# h/t https://github.com/oxalica/rust-overlay/tree/master/examples/cross-aarch64
+# When invoking with `nix-shell`, add "rust-overlay=/path/to/rust-overlay/dir"
+# to $NIX_PATH
+(import <nixpkgs> {
+  crossSystem = {
+    config = "x86_64-w64-mingw32";
+  };
+  overlays = [ (import <rust-overlay>) ];
+}).callPackage (
+{ mkShell, stdenv, rust-bin, windows, wine64, pkg-config }:
+mkShell {
+  nativeBuildInputs = [
+    rust-bin.stable."1.63.0".minimal
+    pkg-config
+  ];
+
+  depsBuildBuild = [ wine64 ];
+
+  buildInputs = [ windows.pthreads ];
+
+  CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
+  CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = "wine64";
+}) {}
